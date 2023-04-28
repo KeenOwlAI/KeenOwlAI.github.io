00f1018ee5340fad4df150296f736a03086ac0e0
KeenOwlAI <keenowlbooking@gmail.com>
2023-04-28 19:51:37 +0200

    First version of my Website

---
+++ src/components/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import {Tilt} from "react-tilt";
+import {motion} from "framer-motion";
+
+import {styles} from "../styles.js";
+import {services} from "../constants/index.js";
+import {fadeIn, textVariant} from "../utils/motion.js";
+import sectionWrapper from "../hoc/SectionWrapper.jsx";
+const ServiceCard = ({index, title, icon}) => {
+  return (
+      <Tilt className={"xs:w-[250px] w-full"}>
+        <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className={"w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"}
+        >
+        <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className={"bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"}
+        >
+        <img src={icon} alt={title}
+             className={"w-16 h-16 object-contain"} />
+            <h3 className={"text-white text-[20px] font-bold text-center"}>{title}</h3>
+        </div>
+        </motion.div>
+        {title}</Tilt>
+  )
+}
+const About = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview</h2>
+    </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className={"mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"}
+      >
+        We are a group of passionate music engineers specialized in the organization of events.
+        We study each need and provide the best possible solution depending on your budget.
+        Let's work together and make your event a success.
+      </motion.p>
+      <div className={"mt-20 flex flex-wrap gap-10"}>
+        {services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+            ))}
+      </div>
+    </>
+  )
+}
+
+export default sectionWrapper (About, "about")
